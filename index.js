306f007e9d846e2fe783ce7028f4434b2225e9e0
yuzusi0 <shion.kaida@gmail.com>
2025-03-10 17:28:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'; // ESモジュール形式
-import { SlashCommandBuilder } from '@discordjs/builders'; // スラッシュコマンドを作成するために必要
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();  // dotenvをインポートして環境変数を読み込む
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // ギルドのイベントを処理する
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! (${c.user.tag})`); // ボットが起動した時に表示
+    console.log(`Ready! (${c.user.tag})`); // 起動した時に"Ready!"とBotの名前をコンソールに出力する
 });
 
-// グローバルコマンドの登録
-client.on(Events.ClientReady, async () => {
-    try {
-        // グローバルコマンドとして登録
-        await client.application.commands.set([
-            new SlashCommandBuilder().setName('mach_diplomacy').setDescription('対戦お相手決まりました！'),
-            new SlashCommandBuilder().setName('mach_information').setDescription('交流戦お相手募集します！')
-        ]);
-        console.log('グローバルコマンドが正常に登録されました');
-    } catch (error) {
-        console.error('コマンド登録エラー:', error);
+client.on(Events.MessageCreate, message => {
+    if (message.author.bot) return; // Botには反応しないようにする
+    if (message.content.includes("!mach_d")) {
+        message.channel.send("@\n対戦お相手決まりました！\nBCvs\n05op\nhost yuzusio\n生存確認含みます！！");
     }
-});
-
-// スラッシュコマンドの処理
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isCommand()) return; // スラッシュコマンドのみ処理する
-
-    const { commandName } = interaction;
-
-    if (commandName === 'mach_diplomacy') {
-        // /mach_diplomacy コマンドが呼ばれた場合
-        await interaction.reply("@\n対戦お相手決まりました！\nBCvs\n05op\nhost yuzusio\n生存確認含みます！！");
-    } else if (commandName === 'mach_information') {
-        // /mach_information コマンドが呼ばれた場合
-        await interaction.reply("時交流戦お相手募集します！\nこちらBC\n主催こちら持てます\n平均mmr\nSorry, Japanese clan only\n#mkmg");
+    if (message.content.includes("!mach_i")) {
+        message.channel.send("時交流戦お相手募集します！\nこちらBC\n主催こちら持てます\n平均mmr\nSorry, Japanese clan only\n#mkmg");
     }
 });
 
