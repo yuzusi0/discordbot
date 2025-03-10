00f9f1d884cba1b54a52e07b189d083df379dc67
yuzusi0 <shion.kaida@gmail.com>
2025-03-10 17:16:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders'); // スラッシュコマンドを作成するために必要
-const { token } = process.env; // トークンを環境変数から取得
+import { Client, Events, GatewayIntentBits } from 'discord.js'; // ESモジュール形式
+import { SlashCommandBuilder } from '@discordjs/builders'; // スラッシュコマンドを作成するために必要
+import { token } from './config.js'; // トークンを環境変数または別ファイルから取得
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // メッセージコンテンツの権限は必要ないので削除
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // ギルドのイベントを処理する
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! (${c.user.tag})`); // 起動時に"Ready!"とBotの名前を表示
+    console.log(`Ready! (${c.user.tag})`); // ボットが起動した時に表示
 });
 
 // グローバルコマンドの登録
 client.on(Events.ClientReady, async () => {
-    // グローバルコマンドとして登録
-    await client.application.commands.set([
-        new SlashCommandBuilder().setName('mach_diplomacy').setDescription('対戦お相手決まりました！'),
-        new SlashCommandBuilder().setName('mach_information').setDescription('交流戦お相手募集します！')
-    ]);
+    try {
+        // グローバルコマンドとして登録
+        await client.application.commands.set([
+            new SlashCommandBuilder().setName('mach_diplomacy').setDescription('対戦お相手決まりました！'),
+            new SlashCommandBuilder().setName('mach_information').setDescription('交流戦お相手募集します！')
+        ]);
+        console.log('グローバルコマンドが正常に登録されました');
+    } catch (error) {
+        console.error('コマンド登録エラー:', error);
+    }
 });
 
 // スラッシュコマンドの処理
@@ -23,14 +28,15 @@ client.on(Events.InteractionCreate, async interaction => {
 
     const { commandName } = interaction;
 
-    if (commandName === 'mach_dipromacy') {
-        // /mach_d コマンドが呼ばれた場合
+    if (commandName === 'mach_diplomacy') {
+        // /mach_diplomacy コマンドが呼ばれた場合
         await interaction.reply("@\n対戦お相手決まりました！\nBCvs\n05op\nhost yuzusio\n生存確認含みます！！");
     } else if (commandName === 'mach_information') {
-        // /mach_i コマンドが呼ばれた場合
+        // /mach_information コマンドが呼ばれた場合
         await interaction.reply("時交流戦お相手募集します！\nこちらBC\n主催こちら持てます\n平均mmr\nSorry, Japanese clan only\n#mkmg");
     }
 });
 
 // トークンでログイン
 client.login(token);
+
