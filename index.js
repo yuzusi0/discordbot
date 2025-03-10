8601a530cfbb8f44a500706632bdc2997bc29717
yuzusi0 <shion.kaida@gmail.com>
2025-03-10 17:10:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders'); // スラッシュコマンドを作成するために必要
+const { token } = process.env; // トークンを環境変数から取得
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // メッセージコンテンツの権限は必要ないので削除
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! (${c.user.tag})`); // 起動した時に"Ready!"とBotの名前をコンソールに出力する
+    console.log(`Ready! (${c.user.tag})`); // 起動時に"Ready!"とBotの名前を表示
 });
 
-client.on(Events.MessageCreate, message => {
-    if (message.author.bot) return; // Botには反応しないようにする
-    if (message.content.includes("!mach_d")) {
-        message.channel.send("@\n対戦お相手決まりました！\nBCvs\n05op\nhost yuzusio\n生存確認含みます！！");
-    }
-    if (message.content.includes("!mach_i")) {
-        message.channel.send("時交流戦お相手募集します！\nこちらBC\n主催こちら持てます\n平均mmr\nSorry, Japanese clan only\n#mkmg");
+// グローバルコマンドの登録
+client.on(Events.ClientReady, async () => {
+    // グローバルコマンドとして登録
+    await client.application.commands.set([
+        new SlashCommandBuilder().setName('mach_diplomacy').setDescription('対戦お相手決まりました！'),
+        new SlashCommandBuilder().setName('mach_information').setDescription('交流戦お相手募集します！')
+    ]);
+});
+
+// スラッシュコマンドの処理
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isCommand()) return; // スラッシュコマンドのみ処理する
+
+    const { commandName } = interaction;
+
+    if (commandName === 'mach_dipromacy') {
+        // /mach_d コマンドが呼ばれた場合
+        await interaction.reply("@\n対戦お相手決まりました！\nBCvs\n05op\nhost yuzusio\n生存確認含みます！！");
+    } else if (commandName === 'mach_information') {
+        // /mach_i コマンドが呼ばれた場合
+        await interaction.reply("時交流戦お相手募集します！\nこちらBC\n主催こちら持てます\n平均mmr\nSorry, Japanese clan only\n#mkmg");
     }
 });
 
-// 環境変数からトークンを読み込み、ログイン
-client.login(process.env.TOKEN);
-
+// トークンでログイン
+client.login(token);
